90cb357e2d4bfc77f56022a18bde01622b0656e0
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-03-11 17:45:31 -0400

    Update apod_api.py
    Import argv

--- apod_api.py
+++ apod_api.py
@@ -1,6 +1,8 @@
 '''
 Library for interacting with NASA's Astronomy Picture of the Day API.
 '''
+# Import statements
+from sys import argv
 
 def main():
     # TODO: Add code to test the functions in this module
