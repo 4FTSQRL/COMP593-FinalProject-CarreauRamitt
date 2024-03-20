2a811ea0a4c00e1144749f595a2c54731d9d90a6
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-03-19 23:01:15 -0400

    apod_api.py updates
    completed get_apod_info() function

--- apod_api.py
+++ apod_api.py
@@ -3,7 +3,13 @@ Library for interacting with NASA's Astronomy Picture of the Day API.
 '''
 # Import statements
 import requests
+import json
 
+# Constants
+# APOD's URL
+APOD_URL = 'https://api.nasa.gov/planetary/apod'
+# API Key
+API_KEY = 'U7HLRY5ArCpjWIorpQN3fx4INNzCpVXkuNv4jYgw'
 
 # Main Function
 def main():
@@ -23,7 +29,37 @@ def get_apod_info(apod_date):
     # TODO: Complete the function body
     # Hint: The APOD API uses query string parameters: https://requests.readthedocs.io/en/latest/user/quickstart/#passing-parameters-in-urls
     # Hint: Set the 'thumbs' parameter to True so the info returned for video APODs will include URL of the video thumbnail image 
-    return
+    
+    # Set parameters
+    params = {'date': apod_date, 'thumbs': True, 'api_key': API_KEY}
+    
+    # Resp message
+    respMsg = requests.get(APOD_URL, params=params)
+    
+    # Update user
+    print(f"Getting {apod_date} APOD information from NASA...", end="")
+    
+    # Check if successful
+    if respMsg.status_code == requests.codes.ok:
+        # Tell user it was a success
+        print(f"success")
+        
+        # Convert to a dictionary
+        dictAPOD = json.loads(respMsg.content)
+        
+        # Return dictionary
+        return dictAPOD
+    
+    # Else statement
+    else:
+        # Tell user it was a failure
+        print(f"failure")
+        
+        # Notify user what the reason was
+        print(f"{respMsg.status_code} {respMsg.reason} {respMsg.text}")
+    
+        # Return None
+        return None
 
 def get_apod_image_url(apod_info_dict):
     """Gets the URL of the APOD image from the dictionary of APOD information.
