6247fa74ac040bdc2d217259af9cc7d789bd4cf8
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-04-15 11:41:33 -0400

    Update apod_viewer.py
    Formatting

--- apod_viewer.py
+++ apod_viewer.py
@@ -18,22 +18,30 @@ root.title("APOD Viewer")
 
 
 #frame for image (top frame)
+root.columnconfigure(0,weight=2)
+root.columnconfigure(1,weight=1)
+root.rowconfigure(0,weight=1)
+root.rowconfigure(1,weight=1)
+    
+    
 image_frame = ttk.Frame(root)
-image_frame.grid(row=0, column=0, padx=20, pady=20)
+
+image_frame.grid(row=0, column=0, columnspan=2)
+
 #top fram widget
 
 
 #frame for title and date (middle frame)
-middle_frame = LabelFrame(root, text="", padx=10, pady=10) #This will need to be pulled from the API
-middle_frame.grid(row=1, column=0, padx=10, pady=10)
+# middle_frame = LabelFrame(root, text="") #This will need to be pulled from the API
+# middle_frame.grid(row=1, column=0)
 
 #frame for cached image and set button (bottom left frame)
-bottom_left_frame = LabelFrame(root, text="View Cached Image", padx=10, pady=10)
-bottom_left_frame.grid(row=2, column=0, padx=10, pady=10)
+bottom_left_frame = LabelFrame(root, text="View Cached Image")
+bottom_left_frame.grid(row=1, column=0)
 
 #frame for date entry and get download image button (bottom right frame)
-bottom_right_frame = LabelFrame(root, text="Get More Images", padx=10, pady=10)
-bottom_right_frame.grid(row=2, column=1, padx=10, pady=10)
+bottom_right_frame = LabelFrame(root, text="Get More Images")
+bottom_right_frame.grid(row=1, column=1)
 
 # TODO: Set Icon
 iconID = 'COMP593.APODViewer'
@@ -56,4 +64,11 @@ selImgcmbx.grid()
 # TODO: Set as Desktop button
 setDskBtn = Button(bottom_left_frame, text="Set as Desktop")
 setDskBtn.grid(row=1,column=1, padx=10)
+
+# TODO: Select Date Label
+selDateLbl = Label(bottom_right_frame, text="Select Date:")
+selDateLbl.grid()
+# TODO: Download Image Button
+dwnldImgBtn = Button(bottom_right_frame, text="Download Image")
+dwnldImgBtn.grid(row=1, column=1, pady=10)
 root.mainloop()
